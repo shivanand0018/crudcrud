5c09a052d1805571649444fc10f787266811c081
shivanand0018 <101448498+shivanand0018@users.noreply.github.com>
2023-02-19 18:36:07 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,43 +1,39 @@
-function submit1(event)
-{
+
+function submit1(event) {
     event.preventDefault();
-    let a={
-        firstName:document.getElementById('FirstName').value,
-        lastName:document.getElementById('lastName').value,
-        email:document.getElementById('Email').value
+    let a = {
+        firstName: document.getElementById('FirstName').value,
+        lastName: document.getElementById('lastName').value,
+        email: document.getElementById('Email').value
     }
-    axios.post('https://crudcrud.com/api/153c27c126644367ad7a94ff6a6ea000/data',a).then((res)=>{
+    axios.post('https://crudcrud.com/api/153c27c126644367ad7a94ff6a6ea000/data', a).then((res) => {
         console.log(res);
     })
-    
+
+
 }
 
-    // function show(a)
-    // {
-    //     const parent=document.getElementById('listOfItems')
-    //     const child=document.createElement('li')
-    //     var btn=document.createElement('button')
-        
-    //     child.textContent=a.firstName+ "-"+a.lastName+"-"+a.email;
-    //     btn.appendChild(document.createTextNode('Delete'))
-    //     btn.onclick=()=>{
-    //         localStorage.removeItem(a.email)
-    //         parent.removeChild(child)
-    //     }
-    //     var btn1=document.createElement('button')
-    //     btn1.appendChild(document.createTextNode('EDIT'))
-    //     btn1.onclick=()=>{
-            
-    //         localStorage.removeItem(a.email)
-    //         parent.removeChild(child)
-    //         document.getElementById('FirstName').value=a.firstName;
-    //         document.getElementById('lastName').value=a.lastName;
-    //         document.getElementById('Email').value=a.email;
-    //     }
-    //     child.appendChild(btn1)
-    //     child.appendChild(btn);
-    //     parent.appendChild(child);
-    // }
+
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get('https://crudcrud.com/api/153c27c126644367ad7a94ff6a6ea000/data').then((res) => {
+        console.log(res.data);
+
+
+        for (let i = 0; i < res.data.length; i++) {
+            const parent = document.getElementById('listOfItems')
+            const child = document.createElement('li')
+            var btn = document.createElement('button')
+
+            child.textContent = res.data[i].firstName + "-" + res.data[i].lastName + "-" + res.data[i].email;
+            btn.appendChild(document.createTextNode('Delete'))
+            var btn1 = document.createElement('button')
+            btn1.appendChild(document.createTextNode('EDIT'))
+            child.appendChild(btn1)
+            child.appendChild(btn);
+            parent.appendChild(child);
+        }
+    })
+})
 
 
 
