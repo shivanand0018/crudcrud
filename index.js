8aed43c548592afd0632947c22f676a95dbff267
shivanand0018 <101448498+shivanand0018@users.noreply.github.com>
2023-02-20 09:56:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function submit1(event) {
         lastName: document.getElementById('lastName').value,
         email: document.getElementById('Email').value
     }
-    axios.post('https://crudcrud.com/api/153c27c126644367ad7a94ff6a6ea000/data', a).then((res) => {
+    axios.post('https://crudcrud.com/api/8d3b0a7f6413476da3ede088eb3553a4/data', a).then((res) => {
         console.log(res);
     })
 
@@ -15,7 +15,7 @@ function submit1(event) {
 
 
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get('https://crudcrud.com/api/153c27c126644367ad7a94ff6a6ea000/data').then((res) => {
+    axios.get('https://crudcrud.com/api/8d3b0a7f6413476da3ede088eb3553a4/data').then((res) => {
         console.log(res.data);
 
 
@@ -26,13 +26,42 @@ window.addEventListener("DOMContentLoaded", () => {
 
             child.textContent = res.data[i].firstName + "-" + res.data[i].lastName + "-" + res.data[i].email;
             btn.appendChild(document.createTextNode('Delete'))
-            btn.onclick=(()=>{
+            btn.onclick = (() => {
                 console.log(res.data[i]._id);
-                axios.delete(`https://crudcrud.com/api/153c27c126644367ad7a94ff6a6ea000/data/${res.data[i]._id}`)
-                document.location.reload();
+                axios.delete(`https://crudcrud.com/api/8d3b0a7f6413476da3ede088eb3553a4/data/${res.data[i]._id}`).then(() => {
+                    document.location.reload();
+                })
+
             })
             var btn1 = document.createElement('button')
             btn1.appendChild(document.createTextNode('EDIT'))
+            btn1.onclick = (() => {
+                axios.get(`https://crudcrud.com/api/8d3b0a7f6413476da3ede088eb3553a4/data/${res.data[i]._id}`).then((data) => {
+                    console.log(data);
+                    document.getElementById('FirstName').value = data.data.firstName;
+                    document.getElementById('lastName').value = data.data.lastName;
+                    document.getElementById('Email').value = data.data.email;
+
+                    let b = {
+                        firstName: document.getElementById('FirstName').value,
+                        lastName: document.getElementById('lastName').value,
+                        email: document.getElementById('Email').value
+                    }
+                    axios.delete(`https://crudcrud.com/api/8d3b0a7f6413476da3ede088eb3553a4/data/${data.data[i]._id}`).then(() => {
+
+                        
+                    })
+
+                    axios.post(`https://crudcrud.com/api/8d3b0a7f6413476da3ede088eb3553a4/data`, b).then(() => {
+
+                        })
+
+
+                    
+
+                })
+
+            })
             child.appendChild(btn1)
             child.appendChild(btn);
             parent.appendChild(child);
